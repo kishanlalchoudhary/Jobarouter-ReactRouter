b1ce91be603e2c434e353177f05ba83c729ca8a5
Kishanlal Choudhary <kisanchoudhary000@gmail.com>
2023-03-19 01:02:03 +0530

    lesson-3

--- src/App.js
+++ src/App.js
@@ -1,32 +1,28 @@
-// import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 
 // pages
 import Home from "./pages/Home";
 import About from "./pages/About";
 
+// layouts
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+    </Route>
+  )
+);
+
 function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Jobarouter</h1>
-          {/* The NavLink is more useful than Link because it adds active class automatically to the link clicked */}
-          {/* <Link to="/">Home</Link> */}
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="about">About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          {/* Below are two methods for the path to root  */}
-          {/* <Route path="/" element={<Home />} /> */}
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
